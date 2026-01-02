32f06b427eb03358ce35ce166a4071981191faba
10148120 <seonghwa.kim@kt.com>
2026-01-02 15:15:07 +0900

    feat: 최초 커밋. 환경 셋업 등

---
+++ src/main/java/org/spark/ai/gourmetagent/GourmetagentApplication.java
@@ -0,0 +1,13 @@
+package org.spark.ai.gourmetagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GourmetagentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GourmetagentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/spark/ai/gourmetagent/GourmetagentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spark.ai.gourmetagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GourmetagentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
